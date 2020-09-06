ca353a3b882ec2edc6064eea325fbd710d5d4d0c
wangsenmu <wangmus@sce-re.com>
2020-09-06 21:11:27 +0800

    leetcode

--- src/main/java/com/wsm/concurrent/executor/ExecutorCase.java
+++ src/main/java/com/wsm/concurrent/executor/ExecutorCase.java
@@ -1,61 +1,61 @@
-package com.wsm.concurrent.executor;
-
-import com.sun.corba.se.impl.transport.SelectorImpl;
-import com.sun.nio.file.ExtendedOpenOption;
-
-import java.io.IOException;
-import java.nio.channels.Channel;
-import java.nio.channels.FileChannel;
-import java.nio.channels.Selector;
-import java.nio.file.OpenOption;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.locks.AbstractQueuedSynchronizer;
-
-/**
- * Created by wangsm on 2018/5/30.
- */
-public class ExecutorCase {
-
-
-
-    private static Executor executor = Executors.newFixedThreadPool(10);
-
-
-
-
-    public static void main(String[] args) {
-
-
-        try {
-            Selector selector = Selector.open();
-
-//            FileChannel fileChannel = FileChannel.open("", ExtendedOpenOption.NOSHARE_DELETE)
-
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        for (int i = 0; i < 20; i++) {
-
-            executor.execute(new Task());
-
-        }
-
-
-
-
-    }
-
-
-    static class Task implements Runnable {
-        public void run() {
-            System.out.println(Thread.currentThread().getName());
-        }
-    }
-
-}
+//package com.wsm.concurrent.executor;
+//
+//import com.sun.corba.se.impl.transport.SelectorImpl;
+//import com.sun.nio.file.ExtendedOpenOption;
+//
+//import java.io.IOException;
+//import java.nio.channels.Channel;
+//import java.nio.channels.FileChannel;
+//import java.nio.channels.Selector;
+//import java.nio.file.OpenOption;
+//import java.util.concurrent.Executor;
+//import java.util.concurrent.Executors;
+//import java.util.concurrent.ThreadFactory;
+//import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+//
+///**
+// * Created by wangsm on 2018/5/30.
+// */
+//public class ExecutorCase {
+//
+//
+//
+//    private static Executor executor = Executors.newFixedThreadPool(10);
+//
+//
+//
+//
+//    public static void main(String[] args) {
+//
+//
+//        try {
+//            Selector selector = Selector.open();
+//
+////            FileChannel fileChannel = FileChannel.open("", ExtendedOpenOption.NOSHARE_DELETE)
+//
+//
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//        for (int i = 0; i < 20; i++) {
+//
+//            executor.execute(new Task());
+//
+//        }
+//
+//
+//
+//
+//    }
+//
+//
+//    static class Task implements Runnable {
+//        public void run() {
+//            System.out.println(Thread.currentThread().getName());
+//        }
+//    }
+//
+//}
